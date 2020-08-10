d3d030283e4b840217359173634e5dc7e6a5df0c
kozu-pid <kozuka1997@icloud.com>
2020-08-10 22:55:36 +0900

    TitleとSelect画面適当に作った

---
+++ sinobikun/Assets/Scripts/SceneController/SelectController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectController : MonoBehaviour
+{
+
+    [SerializeField] private PopButton _popButton;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _popButton.Init(onButtonDownAction);
+    }
+
+    private void onButtonDownAction()
+    {
+        SceneManager.LoadSceneAsync("GameScene");
+    }
+}
---
+++ sinobikun/Assets/Scripts/UIElements/PopButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.UI;
+using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// ポップに押されるボタン
+/// </summary>
+public class PopButton : MonoBehaviour
+{
+    Tweener tweener = null;
+    private Action _onButtonUpAction;
+
+    public void Init(Action onButtonUpAction)
+    {
+        _onButtonUpAction = onButtonUpAction;
+        if (tweener != null)
+        {
+            tweener.Kill();
+            tweener = null;
+            transform.localScale = Vector3.one;
+        }
+    }
+
+    public void OnButtonDownAction()
+    {
+        tweener = transform.DOPunchScale(
+            punch: Vector3.one * 0.1f,
+            duration: 0.2f,
+            vibrato: 1
+        ).SetEase(Ease.OutExpo);
+    }
+
+    public void OnButtonUpAction()
+    {
+        _onButtonUpAction();
+    }
+}
