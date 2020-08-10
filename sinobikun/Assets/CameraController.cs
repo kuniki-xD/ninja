f270b4a34c188ce7305df38f0c357d67c71a7d7e
黒田亮介 <>
2020-08-10 23:00:10 +0900

    no message

---
+++ sinobikun/Assets/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player;
+    void Start()
+    {
+        this.player=GameObject.Find("sinobi");
+    }
+
+    void Update()
+    {
+        Vector3 playerPos=this.player.transform.position;
+        transform.position=new Vector3(playerPos.x,transform.position.y,transform.position.z);
+    }
+}
---
+++ sinobikun/Assets/EnemyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public int health;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        health--;
+        if(health<=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
