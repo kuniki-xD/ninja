44c6973c73287385230d2ffcff88cbfd02db480e
黒田亮介 <>
2020-08-15 14:10:35 +0900

    no message

--- sinobikun/Assets/EnemyController.cs
+++ sinobikun/Assets/EnemyController.cs
@@ -20,11 +20,11 @@ public class EnemyController : MonoBehaviour
         transform.position=new Vector3(Mathf.Sin(Time.time)*3.0f+targetpos.x,targetpos.y,targetpos.z);
         if(Mathf.Sin(Time.time)<=0)
         {
-            transform.localScale=new Vector3(1,transform.localScale.y,transform.localScale.z);
+            transform.localScale=new Vector3(transform.localScale.x*1,transform.localScale.y,transform.localScale.z);
         }
         if(Mathf.Sin(Time.time)>=0)
         {
-            transform.localScale=new Vector3(-1,transform.localScale.y,transform.localScale.z);
+            transform.localScale=new Vector3(transform.localScale.x*-1,transform.localScale.y,transform.localScale.z);
         }
     }   
 
